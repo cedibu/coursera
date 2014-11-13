ae1986bace6b5b7b55aa4ccf069ab89ff50491db
cedricb <cedric.bulloni@gmail.com>
2014-11-13 14:57:53 +0100

    Exercice 3_1

--- 3_1_Philatelie/src/Philatelie.java
+++ 3_1_Philatelie/src/Philatelie.java
@@ -16,8 +16,148 @@ class Timbre {
     /*******************************************
      * Completez le programme a partir d'ici.
      *******************************************/
-}
+    protected String code;
+    protected int annee;
+    protected String pays;
+    protected double valeurFaciale;
+    
+    public Timbre() {
+    	this(CODE_DEFAUT, ANNEE_COURANTE,PAYS_DEFAUT, VALEUR_TIMBRE_DEFAUT);	
+	}
+    
+    public Timbre(String code) {
+    	this(code, ANNEE_COURANTE,PAYS_DEFAUT, VALEUR_TIMBRE_DEFAUT);	
+    }
+    
+    public Timbre(String code, int annee) {
+    	this(code, annee,PAYS_DEFAUT, VALEUR_TIMBRE_DEFAUT);
+	}
+    
+    public Timbre(String code, int annee, String pays) {
+    	this(code, annee,pays, VALEUR_TIMBRE_DEFAUT);
+	}    
+    public Timbre(String code, int annee, String pays, double valeurFaciale) {
+    	this.valeurFaciale = valeurFaciale;
+    	this.pays = pays;
+    	this.annee = annee;
+    	this.code = code;
+	}
+    
+	public String getCode() {
+		return code;
+	}
+
+	public int getAnnee() {
+		return annee;
+	}
+
+	public double getValeurFaciale() {
+		return valeurFaciale;
+	}
 	
+	public String getPays() {
+		return pays;
+	}
+
+	protected double vente() {
+		double age = Double.valueOf(age());
+		if (age<5) {
+			return getValeurFaciale();
+		} else {
+			return getValeurFaciale() * age * 2.5;
+		}
+	}
+	
+	@Override
+	public String toString() {
+		return "Timbre de code "+getCode()+" datant de "+getAnnee()+" (provenance "+getPays()+") ayant pour valeur faciale "+getValeurFaciale()+" francs";
+	}
+	
+	protected int age() {
+		return ANNEE_COURANTE-this.annee;
+	}
+}
+
+class Rare extends Timbre {
+	private int exemplaires;
+	
+	public Rare(String code, int annee,String pays, double valeurFaciale, int exemplaires) {
+		super(code, annee, pays, valeurFaciale);
+		this.exemplaires = exemplaires;
+	}
+	
+	public Rare(String code, int annee,String pays, int exemplaires) {
+		super(code, annee, pays);
+		this.exemplaires = exemplaires;
+	}
+	
+	public Rare(String code, int annee, int exemplaires) {
+		super(code, annee);
+		this.exemplaires = exemplaires;
+	}
+	
+	public Rare(String code, int exemplaires) {
+		super(code);
+		this.exemplaires = exemplaires;
+	}
+	
+	public Rare(int exemplaires) {
+		super();
+		this.exemplaires = exemplaires;
+	}
+	
+	public int getExemplaires() {
+		return exemplaires;
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString() + "\nNombre d'exemplaires -> "+getExemplaires();
+	}
+	
+	@Override
+	protected double vente() {
+		double prixBase = PRIX_BASE_3;
+		double age = Double.valueOf(age());
+		
+		if (exemplaires < BASE_1_EXEMPLAIRES) {
+			prixBase = PRIX_BASE_1;		
+		} else if (exemplaires >= BASE_1_EXEMPLAIRES && exemplaires <=BASE_2_EXEMPLAIRES) {
+			prixBase = PRIX_BASE_2;
+		} 
+		return prixBase * (age / 10);
+	}
+}
+
+class Commemoratif extends Timbre {
+	
+	public Commemoratif(String code, int annee, String pays, double valeurFaciale) {
+		super(code, annee, pays, valeurFaciale);
+	}
+	
+	public Commemoratif(String code, int annee, String pays) {
+		super(code, annee, pays);
+	}
+	
+	public Commemoratif(String code, int annee) {
+		super(code, annee);
+	}
+	
+	public Commemoratif(String code) {
+		super(code);
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + "\nTimbre celebrant un evenement";
+	}
+	
+	@Override
+	protected double vente() {
+		return super.vente() * 2;
+	}
+	
+}
 /*******************************************
  * Ne rien modifier apres cette ligne.
  *******************************************/
